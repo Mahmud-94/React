39f4c535e13ca389286abc34922584aa452d5019
Siyam115 <siyamhasan115@gmail.com>
2024-10-23 13:39:18 +0600

    23 oct

--- project4/src/App4.js
+++ project4/src/App4.js
@@ -1,29 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-export default function App4 () {
-  const [data, setData] =  useState();
+export default function App4() {
+  const [data, setData] = useState([]);
 
-  useEffect(()=> {
-    getData()}, [])
+  useEffect(() => {
+    getData()
+  }, [])
 
   function getData() {
     axios.get(`https://jsonplaceholder.typicode.com/posts`)
 
-    .then(result => {
-  
-      const mydata = result.data;
-  
-      setData({mydata});
-  
-    })
-  }
- 
-console.log(data);
+      .then(result => {
 
-  
+        const mydata = result.data;
+
+    setData(mydata);
+
+      })
+  }
+
+  // console.log(data);
+
+
 
   return (
-    <div>Data Display</div>
+    <>
+      <div>Data Display</div>
+
+      {data.map((item)=>{
+        return (
+        <div key={item.id}>
+          <h2 >{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+        )
+
+      })}
+
+    </>
   )
 }
